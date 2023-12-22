822c2f62914c093614aeb1b93e09cca04c0066cd
Mohamad Rahmani <rahmanipour.mohammad@gmail.com>
2023-12-22 12:59:11 +0330

    working

--- Tests/TCRDemoTests/TCRDemoTests.swift
+++ Tests/TCRDemoTests/TCRDemoTests.swift
@@ -2,11 +2,5 @@ import XCTest
 @testable import TCRDemo
 
 final class TCRDemoTests: XCTestCase {
-    func test_shouldReturnTrue() {
-        XCTAssertTrue(true)
-    }
-
-    func test_shouldFail() {
-        XCTAssertFalse(true)
-    }
+    func test_shouldReturnTrue() throws {}
 }
